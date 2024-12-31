f6c9110ac9eca274a25d448e1df352f2c85bc065
LynsGeekMode <marilyn.triunfo@gmail.com>
2024-12-31 11:30:49 +0100

    added Text for Search not found

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -8,6 +8,10 @@ const CardList = ({ robots }) => {
     throw new Error('NOOOOOOOOOOOOOOOOO!')
   }
 
+  if (!robots.length) {
+    return <h1 className="tc b--green bg-lightest-red">That robot does not exist!</h1>
+  }
+
   const cardComponent = robots.map((user, i) => {
     return (
       <Card
