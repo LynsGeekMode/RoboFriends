fbd4befbbcba2f00a866fd220c334c5f96f509c4
LynsGeekMode <marilyn.triunfo@gmail.com>
2025-01-02 13:24:31 +0100

    UseState and UseEffect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 //import { robots } from "./robots";
@@ -6,57 +6,53 @@ import './App.css';
 import Scroll from "../components/Scroll";
 import ErrorBoundaries from "../components/ErrorBoundaries";
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App() {
 
-  componentDidMount() {
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState('')
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => { this.setState({ robots: users }) })
-  }
+      .then(users => { setRobots(users) });
+    console.log(count)
+  }, [count]) // only run when count changes
 
   //this is a made up function
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
   }
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
 
-    /* Steps: 
-      1. Execute the SearchBox().
-        - if a change was detected, then this.onSearchChange will be updated
-          -- onChange={searchChange}
-      2. filteredRobots will be executed:
-        - change the input type to LowerCase
-        - compare by checking if it includes()
-      3. Update the CardList with the filteredRobots
-    */
-
-    if (!robots.length) {
-      return <h1 className="tc">MAGHINTAY KA!</h1>
-    } else {
-      return (
-        <div className="tc" >
-          <h1 className='F1'> RoboFriends </h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundaries>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundaries>
-          </Scroll>
-        </div>
-      )
-    }
+  /* Steps: 
+    1. Execute the SearchBox().
+      - if a change was detected, then this.onSearchChange will be updated
+        -- onChange={searchChange}
+    2. filteredRobots will be executed:
+      - change the input type to LowerCase
+      - compare by checking if it includes()
+    3. Update the CardList with the filteredRobots
+  */
+  if (!robots.length) {
+    return <h1 className="tc">MAGHINTAY KA!</h1>
+  } else {
+    return (
+      <div className="tc" >
+        <h1 className='F1'> RoboFriends </h1>
+        <button onClick={() => setCount(count + 1)}>Click Me!</button>
+        <h2>Number of Clicks: {count}</h2>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundaries>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundaries>
+        </Scroll>
+      </div>
+    )
   }
 }
 
